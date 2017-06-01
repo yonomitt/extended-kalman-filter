4f2e1db80bbdd89b598674dcd5b40b5e61889adb
Yonatan Mittlefehldt <yono@toojuice.com>
2017-06-01 20:58:09 +0200

    Initialize the kalman filter

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -38,22 +38,36 @@ FusionEKF::FusionEKF() {
   H_laser_ = MatrixXd(2, 4);
   Hj_ = MatrixXd(3, 4);
 
-  //measurement covariance matrix - laser
-  R_laser_ << 0.0225, 0,
-        0, 0.0225;
+  // measurement covariance matrix - laser
+  R_laser_ << 0.0225,      0,
+                   0, 0.0225;
 
-  //measurement covariance matrix - radar
-  R_radar_ << 0.09, 0, 0,
-        0, 0.0009, 0,
-        0, 0, 0.09;
+  // measurement covariance matrix - radar
+  R_radar_ << 0.09,      0,    0,
+                 0, 0.0009,    0,
+                 0,      0, 0.09;
 
-  /**
-  TODO:
-    * Finish initializing the FusionEKF.
-    * Set the process and measurement noises
-  */
+  //
+  // Finish initializing the FusionEKF.
+  // Set the process and measurement noises
+  //
 
+  // create a 4D state vector
+  ekf_.x_ = VectorXd(4);
 
+  // state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0,    0,    0,
+             0, 1,    0,    0,
+             0, 0, 1000,    0,
+             0, 0,    0, 1000;
+  
+  // the initial transition matrix F_
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 }
 
 /**
