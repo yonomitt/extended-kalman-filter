e6dd903250fb0a4e1e4eeadad071e2db0e8f54df
Yonatan Mittlefehldt <yono@toojuice.com>
2017-06-02 10:23:38 +0200

    Fill in stub for the predict step of the kalman filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,10 +18,9 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
